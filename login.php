1b356e5486e0b2d3187bc7a06eaecb46eaa5d441
SylvexX <108277123+SylvexX@users.noreply.github.com>
2024-02-25 21:37:33 +0100

    Verder met todo en styles geupdate

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🌿 QuestKeeper 🌿</title>
-    <style><?php include 'style.css';?></style>
+    <link  rel="stylesheet" href="style.css">
     <script src="js/login.js" defer></script>
 </head>
 <body>
--- todo.php
+++ todo.php
@@ -6,12 +6,15 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>-
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🌿 QuestKeeper 🌿</title>
 </head>
 <body>
-
+    <form class="add-item-form">
+        <input type="text" id="add-item-input" placeholder="maak dit ....." required/>
+        <button type="button" id="add-item-btn">Voeg Toe</button>
+    </form>
 </body>
 </html>
